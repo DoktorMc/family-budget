080a107c2d293f39a20ac97835157a7759708c3b
Vitalii Matviienko <doktormc25@gmail.com>
2024-03-07 17:44:48 +0200

    style minor change? create color variable data, work with category edit form

--- src/modules/Settings/components/EditCategoryForm.jsx
+++ src/modules/Settings/components/EditCategoryForm.jsx
@@ -1,48 +1,50 @@
-import React from 'react';
-import CustomButton from './../../../Custom/Button/CustomButton';
-import doneCancelSprite from './../../../img/done-cancel-sprite-ico.svg'
+import React from "react";
+import CustomButton from "./../../../Custom/Button/CustomButton";
+import doneCancelSprite from "./../../../img/done-cancel-sprite-ico.svg";
 
 const EditCategoryForm = ({ categoryForEdit }) => {
-  console.log('Item for edit category', categoryForEdit);
+  console.log("Item for edit category", categoryForEdit);
 
-  const handleChange = () => { }
-  
+  const handleChange = () => {};
+
   return (
-    <div>
-      <div className="manage-categories__table__items__list__item">
-        <div>
-          <label htmlFor="edit_name_category">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="edit_name_category"
-            value={categoryForEdit.category.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="manage-categories__table__items__list__item__buttons">
-          <CustomButton
-            title={
-              <svg className="navbar-icon">
-                <use xlinkHref={doneCancelSprite + "#done_white"} />
-              </svg>
-            }
-            buttonTheme="edit"
-            // handleProp={handleModalOpen}
-          />
-          <CustomButton
-            title={
-              <svg className="navbar-icon">
-                <use xlinkHref={doneCancelSprite + "#close_white"} />
-              </svg>
-            }
-            buttonTheme="delete"
-            // handleProp={}
-          />
-        </div>
+    <div className="edit__category__form">
+      {/* <div className="manage-categories__table__items__list__item"> */}
+
+      <div className="edit__category__form__items">
+        <label htmlFor="edit_name_category">Name of category</label>
+        <input
+          className="edit__category__form__items__input-name"
+          type="text"
+          name="name"
+          id="edit_name_category"
+          defaultValue={categoryForEdit.category.name}
+          onChange={handleChange}
+        />
       </div>
+      <div className="edit__category__form__buttons">
+        <CustomButton
+          title={
+            <svg className="navbar-icon">
+              <use xlinkHref={doneCancelSprite + "#done_white"} />
+            </svg>
+          }
+          buttonTheme="edit"
+          // handleProp={handleModalOpen}
+        />
+        <CustomButton
+          title={
+            <svg className="navbar-icon">
+              <use xlinkHref={doneCancelSprite + "#close_white"} />
+            </svg>
+          }
+          buttonTheme="delete"
+          // handleProp={}
+        />
+      </div>
+      {/* </div> */}
     </div>
   );
-}
+};
 
 export default EditCategoryForm;
